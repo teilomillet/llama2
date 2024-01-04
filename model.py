d6a62a50c6c9bfa3307292edc9ce1a76b65b8294
Teïlo <teilomillet@gmail.com>
2024-01-04 13:59:12 +0100

    EncoderBlock

--- model.py
+++ model.py
@@ -86,7 +86,6 @@ class RMSNorm(nn.Module):
     '''
  
     def __init__(self, dim: int, eps:float = 1-6):
-        
         super().__init__()
         
         self.eps = eps
@@ -101,10 +100,41 @@ class RMSNorm(nn.Module):
     def forward(self, x: torch.Tensor ):
         # (dim) * (B, seq_len, dim) * (B, seq_len, dim)
         return self.weight * self._norm(x.float()).type_as(x)
+    
+    
+class EncoderBlock(nn.Module):
+    
+    def __init__(self, args: ModelArgs) -> None:
+        super().__init__()
+        
+        self.n_heads = args.n_heads
+        self.dim = args.dim
+        self.head_dim = args.dim // args.n_heads
+        
+        self.attention = SelfAttention(args)
+        self.feed_forward = FeedForward(args)
+        
+        # Normalisations
+        
+        # Avant self attention
+        self.attention_norm = RMSNorm(args.dim, eps=args.norm_eps)
+        
+        # Avant feed forward block
+        self.ffn_norm = RMSNorm(args.dim, args.norm_eps)
+        
+    def forward(self, x: torch.Tensor, start_pos: int, freqs_complex: torch.Tensor):
+        # (B, seq_len, dim) + (B, seq_len, dim) -> (B, seq_len, dim)
+        h = x + self.attention.forward(self.attention_norm(x), start_pos, freqs_complex)
+        out = h + self.feed_forward.forward(self.ffn_norm(h))
+        return out
+        
+        
+        
         
  
 class Transformers(nn.Module):
     '''
+    Llama architecture.
     Les noms sont quasiment les mêmes que ceux de llama.
     '''
     
